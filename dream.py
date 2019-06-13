09115da4705ab0dc89c21d43a7fda5d2ef5a49e0
krulvis <joepkleint@gmail.com>
2019-06-13 23:53:24 +0200

    adding inception5h model

--- dream.py
+++ dream.py
@@ -1,9 +1,148 @@
+import math
 import os
+import random
+
+import PIL
 import numpy as np
 import ffmpeg
 
 from tensorflow_stream import DeepDream, run
 from utils import abs_out, output_dir
+
+
+def normalize_image(x):
+    # Get the min and max values for all pixels in the input.
+    x_min = x.min()
+    x_max = x.max()
+
+    # Normalize so all values are between 0.0 and 1.0
+    x_norm = (x - x_min) / (x_max - x_min)
+
+    return x_norm
+
+
+def resize_image(image, size=None, factor=None):
+    # If a rescaling-factor is provided then use it.
+    if factor is not None:
+        # Scale the numpy array's shape for height and width.
+        size = np.array(image.shape[0:2]) * factor
+
+        # The size is floating-point because it was scaled.
+        # PIL requires the size to be integers.
+        size = size.astype(int)
+    else:
+        # Ensure the size has length 2.
+        size = size[0:2]
+
+    # The height and width is reversed in numpy vs. PIL.
+    size = tuple(reversed(size))
+
+    # Ensure the pixel-values are between 0 and 255.
+    img = np.clip(image, 0.0, 255.0)
+
+    # Convert the pixels to 8-bit bytes.
+    img = img.astype(np.uint8)
+
+    # Create PIL-object from numpy array.
+    img = PIL.Image.fromarray(img)
+
+    # Resize the image.
+    img_resized = img.resize(size, PIL.Image.LANCZOS)
+
+    # Convert 8-bit pixel values back to floating-point.
+    img_resized = np.float32(img_resized)
+
+    return img_resized
+
+
+def get_tile_size(num_pixels, tile_size=400):
+    """
+    num_pixels is the number of pixels in a dimension of the image.
+    tile_size is the desired tile-size.
+    """
+
+    # How many times can we repeat a tile of the desired size.
+    num_tiles = int(round(num_pixels / tile_size))
+
+    # Ensure that there is at least 1 tile.
+    num_tiles = max(1, num_tiles)
+
+    # The actual tile-size.
+    actual_tile_size = math.ceil(num_pixels / num_tiles)
+
+    return actual_tile_size
+
+
+def tiled_gradient(session, model, gradient, image, tile_size=400):
+    # Allocate an array for the gradient of the entire image.
+    grad = np.zeros_like(image)
+
+    # Number of pixels for the x- and y-axes.
+    x_max, y_max, _ = image.shape
+
+    # Tile-size for the x-axis.
+    x_tile_size = get_tile_size(num_pixels=x_max, tile_size=tile_size)
+    # 1/4 of the tile-size.
+    x_tile_size4 = x_tile_size // 4
+
+    # Tile-size for the y-axis.
+    y_tile_size = get_tile_size(num_pixels=y_max, tile_size=tile_size)
+    # 1/4 of the tile-size
+    y_tile_size4 = y_tile_size // 4
+
+    # Random start-position for the tiles on the x-axis.
+    # The random value is between -3/4 and -1/4 of the tile-size.
+    # This is so the border-tiles are at least 1/4 of the tile-size,
+    # otherwise the tiles may be too small which creates noisy gradients.
+    x_start = random.randint(-3 * x_tile_size4, -x_tile_size4)
+
+    while x_start < x_max:
+        # End-position for the current tile.
+        x_end = x_start + x_tile_size
+
+        # Ensure the tile's start- and end-positions are valid.
+        x_start_lim = max(x_start, 0)
+        x_end_lim = min(x_end, x_max)
+
+        # Random start-position for the tiles on the y-axis.
+        # The random value is between -3/4 and -1/4 of the tile-size.
+        y_start = random.randint(-3 * y_tile_size4, -y_tile_size4)
+
+        while y_start < y_max:
+            # End-position for the current tile.
+            y_end = y_start + y_tile_size
+
+            # Ensure the tile's start- and end-positions are valid.
+            y_start_lim = max(y_start, 0)
+            y_end_lim = min(y_end, y_max)
+
+            # Get the image-tile.
+            img_tile = image[x_start_lim:x_end_lim,
+                       y_start_lim:y_end_lim, :]
+
+            # Create a feed-dict with the image-tile.
+            feed_dict = model.create_feed_dict(image=img_tile)
+
+            # Use TensorFlow to calculate the gradient-value.
+            g = session.run(gradient, feed_dict=feed_dict)
+
+            # Normalize the gradient for the tile. This is
+            # necessary because the tiles may have very different
+            # values. Normalizing gives a more coherent gradient.
+            g /= (np.std(g) + 1e-8)
+
+            # Store the tile's gradient at the appropriate location.
+            grad[x_start_lim:x_end_lim,
+            y_start_lim:y_end_lim, :] = g
+
+            # Advance the start-position for the y-axis.
+            y_start = y_end
+
+        # Advance the start-position for the x-axis.
+        x_start = x_end
+
+    return grad
+
 
 
 class Dream():
